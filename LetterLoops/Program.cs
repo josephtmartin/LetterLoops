82d517c5ee8f12561f798d245f931dd208d3e803
joe <goldglovejo13@gmail.com>
2021-01-16 10:03:39 -0600

    Add project files.

---
+++ LetterLoops/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace LetterLoops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Type some random characters");
+            var characters = Console.ReadLine();
+            string newString = string.Empty;
+            var counter = 1;
+            foreach (char a in characters)
+            {
+                for (int i = 0; i < counter; i++)
+                {
+                    newString += a;
+                }
+                newString += "-";
+                counter++;
+            }
+            Console.WriteLine($"Here is the new string {newString.TrimEnd('-')}");
+        }
+    }
+}
